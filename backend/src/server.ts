6500e7264970b17570f71671b414ce25c091d75e
Saujanya Tripathi <iamsaujanya.ig@gmail.com>
2024-12-13 06:56:12 +0000

    workflow

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,9 @@ const port = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
-  'https://iamsaujanya.github.io'
+  'https://iamsaujanya.github.io',
+  'https://romind.me',
+  'http://romind.me'
 ];
 
 const corsOptions = {
